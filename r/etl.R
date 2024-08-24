c2ad316305de2b44502e563acec8eca9acbe1c5d
Marcelo Ventura <zyxdef@gmail.com>
2024-08-24 08:24:34 -0300

    commented out e passei para o fim do arquivo

--- r/etl.R
+++ r/etl.R
@@ -1,35 +1,5 @@
 # carregando as bibliotecas ----
 library(tidyverse)
-
-# função parseadora dos htmls ----
-parser_telegram <- function(html_file) {
-  # indicador de progresso ----
-  cat(html_file |> str_extract("messages([0-9]*\\.)", group = 1))
-  # pega todas as mensagens ----
-  divs <- xml2::read_html(html_file) |>
-    xml2::xml_find_all("//div[@class='message default clearfix']")
-  # nome da pessoa ----
-  nomes <- divs |>
-    xml2::xml_find_all("./div/div[@class='from_name']") |>
-    xml2::xml_text() |>
-    stringr::str_squish()
-  # data e hora da mensagem ----
-  data_horas <- divs |>
-    xml2::xml_find_all("./div/div[@class='pull_right date details']") |>
-    xml2::xml_attr("title") |>
-    lubridate::dmy_hms()
-  # texto da mensagem ----
-  textos <- divs |>
-    purrr::map(xml2::xml_find_first, "./div/div[@class='text']") |>
-    purrr::map_chr(xml2::xml_text) |>
-    stringr::str_squish()
-  # retorna numa tabela ----
-  tibble::tibble(
-    data_hora = data_horas,
-    nome = nomes,
-    texto = textos
-  )
-}
 # library(tidytext)  # precisa?
 library(xml2)
 
@@ -63,3 +33,32 @@ df <-
 
 # salva em .rds ----
 saveRDS(df, file = "./data/df.rds")
+# # função parseadora dos htmls ----
+# parser_telegram <- function(html_file) {
+#   # indicador de progresso ----
+#   cat(html_file |> str_extract("messages([0-9]*\\.)", group = 1))
+#   # pega todas as mensagens ----
+#   divs <- read_html(html_file) |>
+#     xml_find_all("//div[@class='message default clearfix']")
+#   # nome da pessoa ----
+#   nomes <- divs |>
+#     xml_find_all("./div/div[@class='from_name']") |>
+#     xml_text() |>
+#     str_squish()
+#   # data e hora da mensagem ----
+#   data_horas <- divs |>
+#     xml_find_all("./div/div[@class='pull_right date details']") |>
+#     xml_attr("title") |>
+#     dmy_hms()
+#   # texto da mensagem ----
+#   textos <- divs |>
+#     map(xml_find_first, "./div/div[@class='text']") |>
+#     map_chr(xml_text) |>
+#     str_squish()
+#   # retorna numa tabela ----
+#   tibble(
+#     data_hora = data_horas,
+#     nome = nomes,
+#     texto = textos
+#   )
+# }
