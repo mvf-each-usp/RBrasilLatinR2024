55d53a4c9472a0e0f1a65b0adff46dda94e076c5
Marcelo Ventura <zyxdef@gmail.com>
2024-08-24 01:21:15 -0300

    "%>%" -> "|>"

--- r/etl.R
+++ r/etl.R
@@ -6,22 +6,22 @@ library(tidytext)
 ####$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 parser_telegram <- function(html_file) {
   # pega todas as mensagens
-  divs <- xml2::read_html(html_file) %>%
+  divs <- xml2::read_html(html_file) |>
     xml2::xml_find_all("//div[@class='message default clearfix']")
   # nome da pessoa
-  nomes <- divs %>%
-    xml2::xml_find_all("./div/div[@class='from_name']") %>%
-    xml2::xml_text() %>%
+  nomes <- divs |>
+    xml2::xml_find_all("./div/div[@class='from_name']") |>
+    xml2::xml_text() |>
     stringr::str_squish()
   # data e hora da mensagem
-  data_horas <- divs %>%
-    xml2::xml_find_all("./div/div[@class='pull_right date details']") %>%
-    xml2::xml_attr("title") %>%
+  data_horas <- divs |>
+    xml2::xml_find_all("./div/div[@class='pull_right date details']") |>
+    xml2::xml_attr("title") |>
     lubridate::dmy_hms()
   # texto da mensagem
-  textos <- divs %>%
-    purrr::map(xml2::xml_find_first, "./div/div[@class='text']") %>%
-    purrr::map_chr(xml2::xml_text) %>%
+  textos <- divs |>
+    purrr::map(xml2::xml_find_first, "./div/div[@class='text']") |>
+    purrr::map_chr(xml2::xml_text) |>
     stringr::str_squish()
   # retorna numa tabela
   tibble::tibble(
