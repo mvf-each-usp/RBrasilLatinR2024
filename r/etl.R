a3ac3e9ec92d6352190dd8b37e6be658731fd8a7
Marcelo Ventura <zyxdef@gmail.com>
2024-08-25 06:07:45 -0300

    o parse não tá mais em forma de função, e sim direto no nodeset

--- r/etl.R
+++ r/etl.R
@@ -22,54 +22,49 @@ mensagens <-
   reduce(xml_add_sibling) |>
   xml_find_all("//div[contains(@class, 'message')]")
 
-parseia <- function(msgs) {
+# parse das mensagens para data frame ----
+df1 <-
   tibble(
-    id = xml_attr(msgs, "id"),
-    classe = xml_attr(msgs, "class"),
     tipo = if_else(str_detect(classe, "service"), "serviço", "postagem"),
+    id = xml_attr(mensagens, "id"),
+    classe = xml_attr(mensagens, "class"),
     autor =
-      msgs |>
+      mensagens |>
       xml_find_first(".//div[@class='from_name']") |>
       xml_text() |>
       str_squish(),
     continuacao = str_detect(classe, "joined"),
     data =
-      msgs |>
+      mensagens |>
       xml_find_first(".//div[@class='pull_right date details']") |>
       xml_attr("title") |>
       dmy_hms(),
     texto =
-      msgs |>
+      mensagens |>
       xml_find_first(".//div[@class='text']") |>
       xml_text() |>
       str_squish(),
     reply.to =
-      msgs |>
+      mensagens |>
       xml_find_first(".//div[@class='reply_to details']/a") |>
       xml_attr("href") |>
       str_squish(),
     entrada =
-      msgs |>
+      mensagens |>
       xml_find_first(".//div[@class='body details']") |>
       xml_text() |>
       str_squish(),
     ### O QUE MAIS DEVERIA PEGAR? ----
+  )
+
   ) |>
-    select(-classe)
-}
 
 
-tudo[1:50] |>
-  parseia()
-# a última coluna não pega a mensagem 26 por exemplo,
-# que é uma mensagem de entrada no grupo
 
 # um problema para resolver mais adiante:
 # a Rose passou a apagar as mensagens de entrada de novo membro;
 # isso vai atrapalhar a acompanhar a evolução do número de membros do grupo
 
-mensagens_bruto <-
-  parseia(tudo)
 
 
 # FAZER AQUI ----
